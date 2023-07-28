0a0de78cefb71b11c7c77a3a555a5c2f0f5a0c29
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-07-28 20:41:06 -0300

    Modifico componente Gradient

---
+++ los_perro_amigo/src/componentes/GradientBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const GradientBackground = () => {
+  return (
+    <div className="gradient-background">
+      <h1>Classes?</h1>
+    </div>
+  );
+};
+
+export default GradientBackground;
