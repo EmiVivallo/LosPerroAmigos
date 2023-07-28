a197e8c0d685e77221350487adf7fc969f48cea7
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-07-28 20:51:46 -0300

    Modificanco App

--- los_perro_amigo/src/App.js
+++ los_perro_amigo/src/App.js
@@ -8,26 +8,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div className="container">
         <RegisterForm />
       </div>
-      return (
         <div>
           <GradientBackground />
         </div>
-  );
+      </header>
+      
     </div>
   );
 }
