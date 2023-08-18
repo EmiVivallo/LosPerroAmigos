79267eda1b90fb7259cd27bac90fe68b37507c1d
EmiVivallo <emivivallozmm@gmail.com>
2023-08-17 22:05:47 -0300

    Termino de crear el componente

--- los-perro-amigos/src/componentes/boton.js
+++ los-perro-amigos/src/componentes/boton.js
@@ -1,1 +1,10 @@
 import React from 'react';
+const Button = ({ label, onClick }) => {
+    return (
+      <button className="custom-button" onClick={onClick}>
+        {label}
+      </button>
+    );
+  };
+
+  export default Button;
