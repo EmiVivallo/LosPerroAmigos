5ac34e814012d71a4c61c5d31c4cbeccabd74f2e
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-08-28 14:29:19 -0300

    Agregando suspense

--- los_perro_amigo/src/index.js
+++ los_perro_amigo/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import firebaseConfig from './firebase-config';
 
 import {
+  FirebaseAppProvider,
   FirebaseAppProviders
 }from 'reactfire'
 
@@ -14,7 +15,11 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={'Conectando la app'}>
+      <App />
+    </Suspense>
+  </FirebaseAppProvider>
   </React.StrictMode>
 );
 
