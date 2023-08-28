46a73ae51f6d519688c4f303a3697f0d33db891a
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-08-28 14:14:56 -0300

    Exportando config firebase

---
+++ los_perro_amigo/src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC5bn_hytd-4dKrm5-aehbf6RrwgBuJHPI",
+    authDomain: "losperroamigos-ef2cc.firebaseapp.com",
+    projectId: "losperroamigos-ef2cc",
+    storageBucket: "losperroamigos-ef2cc.appspot.com",
+    messagingSenderId: "498931176498",
+    appId: "1:498931176498:web:7323b5557471db5366982b",
+    measurementId: "G-KEPL7PVE73"
+  };
