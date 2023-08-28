c290b6a5c0fe8a1223c6d2a6562f8a3f6c3a7742
EmiVivallo <emivivallozmm@gmail.com>
2023-08-28 17:00:28 -0300

    Definp otras 4 aulas

--- los-perro-amigos/src/componentes/AulasComponent.js
+++ los-perro-amigos/src/componentes/AulasComponent.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 function AulasComponent() {
   const aulas = [
     { nombreAula: 'Aula 101', materia: 'Matemáticas' },
+    { nombreAula: 'Aula 102', materia: 'Historia' },
+    { nombreAula: 'Aula 103', materia: 'Ciencias' },
+    { nombreAula: 'Aula 104', materia: 'Inglés' },
+    { nombreAula: 'Aula 105', materia: 'Arte' },
   ];
 
   
