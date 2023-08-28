9ebc9a81729a26a2c4f6583aa463e83f1026c3a4
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-08-28 15:42:58 -0300

    Modificando config

--- los_perro_amigo/src/index.js
+++ los_perro_amigo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebaseConfig from './firebase-config';
+import firebaseConfig from './componentes/firebase-config';
 
 import {
   FirebaseAppProvider
