9157232a4a39c0c92af1604b7b508601224ac20c
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-08-28 14:22:11 -0300

    Importando config firebase

--- los_perro_amigo/src/index.js
+++ los_perro_amigo/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebaseConfig from './firebase-config';
 
 import {
   FirebaseAppProviders
 }from 'reactfire'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
