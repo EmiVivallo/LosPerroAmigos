7c407fbb924cd02f18c4612a20e0446bc5423724
EmiVivallo <emivivallozmm@gmail.com>
2023-08-28 16:58:04 -0300

    Importo react a componente aulas

--- los-perro-amigos/src/componentes/AulasComponent.js
+++ los-perro-amigos/src/componentes/AulasComponent.js
@@ -1,55 +1,6 @@
 import React, { useState } from 'react';
 
-function AulasComponent() {
-  // Definimos un array de objetos que representan las aulas y las materias
-  const aulas = [
-    { nombreAula: 'Aula 101', materia: 'Matemáticas' },
-    { nombreAula: 'Aula 102', materia: 'Historia' },
-    { nombreAula: 'Aula 103', materia: 'Ciencias' },
-    { nombreAula: 'Aula 104', materia: 'Inglés' },
-    { nombreAula: 'Aula 105', materia: 'Arte' },
-  ];
 
-  // Estado para controlar la aula ampliada
-  const [aulaAmpliada, setAulaAmpliada] = useState(null);
 
-  // Manejador de clic para mostrar el aula ampliada
-  const handleAulaClick = (aula) => {
-    setAulaAmpliada(aula);
-  };
 
-  // Manejador de clic para cerrar el aula ampliada
-  const handleCloseClick = () => {
-    setAulaAmpliada(null);
-  };
 
-  return (
-    <div className="AulasComponent"> {/* Asigna la clase al contenedor principal */}
-      <h2 className="AulasComponent-title">Lista de Aulas y Materias</h2> {/* Asigna la clase al título */}
-      <ul className="AulasComponent-list"> {/* Asigna la clase a la lista */}
-        {aulas.map((aula, index) => (
-          <li
-            className="AulasComponent-item"
-            key={index}
-            onClick={() => handleAulaClick(aula)}
-          >
-            <strong>Aula:</strong> {aula.nombreAula}, <strong>Materia:</strong> {aula.materia}
-          </li>
-        ))}
-      </ul>
-
-      {/* Mostrar el aula ampliada */}
-      {aulaAmpliada && (
-        <div className="AulaAmpliada">
-          <div className="AulaAmpliada-content">
-            <h2>{aulaAmpliada.nombreAula}</h2>
-            <p>Materia: {aulaAmpliada.materia}</p>
-            <button onClick={handleCloseClick}>Cerrar</button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default AulasComponent;
