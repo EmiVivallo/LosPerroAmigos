e961bde16ebdbf1bec237cf65ec5b24aa162d828
EmiVivallo <emivivallozmm@gmail.com>
2023-10-17 22:02:18 -0300

    Vuelvo a modificar app.js

--- los-perro-amigos/src/App.js
+++ los-perro-amigos/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import AulasComponent from './componentes/AulasComponent'; 
-import Algo from './componentes/algo'; 
 
 function App() {
   const handleButtonClick = () => {
@@ -12,7 +11,6 @@ function App() {
     <div className="App">
       <button onClick={handleButtonClick}>Mi Botón</button> {}
       <AulasComponent /> {}
-      <Algo /> {}
     </div>
   );
 }
