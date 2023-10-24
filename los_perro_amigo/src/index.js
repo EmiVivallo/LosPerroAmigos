38beeaaae9344f7de10623655b937409da4402d7
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-24 18:49:19 -0300

    Renombrando firebaseconfig

--- los_perro_amigo/src/index.js
+++ los_perro_amigo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import firebaseConfig from './componentes/firebase-config';
+import firebaseConfig from './componentes/firebaseConfig';
 
 import {
   FirebaseAppProvider
@@ -19,7 +19,4 @@ ReactDOM.render((
   </React.StrictMode>
 ), document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
