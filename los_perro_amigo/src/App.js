abe287e64217d5aecd5135de60ab65c4f559efb2
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-24 16:16:18 -0300

    Editando app.js

--- los_perro_amigo/src/App.js
+++ los_perro_amigo/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import './index.js';
 import RegisterForm from './componentes/RegisterForm';
 import GradientBackground from './componentes/GradientBackground';
 import 'firebase/auth';
-import {
-  useFirebaseApp
-} from 'reactfire';
+import { useFirebaseApp } from 'reactfire';
+import firebaseConfig from './componentes/firebase-config'; // Importa la configuración de Firebase
 
 function App() {
   const firebase = useFirebaseApp();
@@ -14,14 +12,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div className="container">
-        <RegisterForm />
-      </div>
+        <div className="container">
+          <RegisterForm />
+        </div>
         <div>
           <GradientBackground />
         </div>
       </header>
-      
     </div>
   );
 }
