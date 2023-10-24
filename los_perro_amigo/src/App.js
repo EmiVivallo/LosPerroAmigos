bce2ec4a0061a6881dac2f441cd8c917ef4d15e7
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-24 19:24:04 -0300

    Reaciendo formulario

--- los_perro_amigo/src/App.js
+++ los_perro_amigo/src/App.js
@@ -36,44 +36,7 @@ import GradientBackground from './componentes/GradientBackground';
 
 function App () {
   return (
-    <div className="form-container">
-      <h2>Registro</h2>
-      <form>
-        <label className="form-label">
-          Nombre de usuario:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="form-input"
-          />
-        </label>
-        <br />
-        <label className="form-label">
-          Correo electrónico:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-input"
-          />
-        </label>
-        <br />
-        <label className="form-label">
-          Contraseña:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-input"
-          />
-        </label>
-        <br />
-        <button type="button" onClick={submit} className="form-button">
-          Registrar
-        </button>
-      </form>
-    </div>
+
   );
 }
 
