fdd68fbcf4d7d96486cbffbc47a62507f5b2d012
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-24 19:46:25 -0300

    Volviendo atras

--- los_perro_amigo/src/App.js
+++ los_perro_amigo/src/App.js
@@ -1,12 +1,10 @@
-/*import React from 'react';
+import React from 'react';
 import './App.css';
 import './index.js';
 import RegisterForm from './componentes/RegisterForm';
 import GradientBackground from './componentes/GradientBackground';
 import 'firebase/auth';
-import {
-  useFirebaseApp
-} from 'reactfire';
+import { useFirebaseApp } from 'reactfire';
 
 function App() {
   const firebase = useFirebaseApp();
@@ -22,24 +20,6 @@ function App() {
         </div>
       </header>
       
-    </div>
-  );
-}
-
-export default App;*/
-
-
-import React from 'react';
-import './App.css';
-import './index.js';
-import GradientBackground from './componentes/GradientBackground';
-import Auth from './auth';
-
-function App () {
-  return (
-    <div>
-      <p>usuario:</p>
-      <Auth />
     </div>
   );
 }
