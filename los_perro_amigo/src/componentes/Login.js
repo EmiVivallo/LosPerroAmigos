bf5b987daed9925ffe8e9e83dd343c80788a3d78
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-31 17:02:07 -0300

    Editando css

--- los_perro_amigo/src/componentes/Login.js
+++ los_perro_amigo/src/componentes/Login.js
@@ -2,27 +2,7 @@ import React from "react"
 
 const Login = () => {
     return (
-        <div className="formu">
-  <form className="form-container">
-    <h2 className="formTit">Registro</h2>
-
-    <div className="form_container">
-      <div className="formGrup">
-        <input type="email" className="formInp" placeholder="" />
-        <label className="formLab">Correo electrónico:</label>
-      </div>
-
-      <div className="formGrup">
-        <input type="password" className="formInp" placeholder="" />
-        <label className="formLab">Contraseña:</label>
-      </div>
-
-      <button type="button" className="formSub boton-ani">Registrar</button>
-    </div>
-  </form>
-</div>
-
-        /*<div className="form-container">
+        <div className="form-container">
                 <h2>Registro</h2>
                 <form>
                   <label className="form-label">
@@ -37,7 +17,7 @@ const Login = () => {
                   <br />
                   <button type="button" className="form-button"> Registrar </button>
                 </form>
-              </div>*/
+              </div>
     )
 }
 
