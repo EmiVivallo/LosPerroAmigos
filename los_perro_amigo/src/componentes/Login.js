6c7485c51e821f010241a16325e4b5cba0aeb13a
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-31 16:05:54 -0300

    Editando css

--- los_perro_amigo/src/componentes/Login.js
+++ los_perro_amigo/src/componentes/Login.js
@@ -2,7 +2,30 @@ import React from "react"
 
 const Login = () => {
     return (
-        <div className="form-container">
+        <div className="formu">
+  <form className="form-container">
+    <h2 className="formTit">Registro</h2>
+
+    <div className="form_container">
+      <div className="formGrup">
+        <input type="email" className="formInp" placeholder="Correo electrónico" />
+        <label className="formLab">Correo electrónico:</label>
+      </div>
+
+      <div className="formGrup">
+        <input type="password" className="formInp" placeholder="Contraseña" />
+        <label className="formLab">Contraseña:</label>
+      </div>
+
+      <button type="button" className="formSub boton-ani">Registrar</button>
+    </div>
+  </form>
+  <div id="formfin">
+    <p>¿Ya tienes una cuenta? <a href="#">Iniciar sesión</a></p>
+  </div>
+</div>
+
+        /*<div className="form-container">
                 <h2>Registro</h2>
                 <form>
                   <label className="form-label">
@@ -17,7 +40,7 @@ const Login = () => {
                   <br />
                   <button type="button" className="form-button"> Registrar </button>
                 </form>
-              </div>
+              </div>*/
     )
 }
 
--- los_perro_amigo/src/index.js
+++ los_perro_amigo/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GradientBackground from './componentes/GradientBackground';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <GradientBackground />
   </React.StrictMode>,
   document.getElementById('root')
 );
