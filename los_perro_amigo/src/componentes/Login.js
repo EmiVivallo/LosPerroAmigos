2f486b94af429e807609e13a2275a7455c7fcdbf
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-10-31 15:39:07 -0300

    Importando formulario de login

--- los_perro_amigo/src/componentes/Login.js
+++ los_perro_amigo/src/componentes/Login.js
@@ -2,7 +2,22 @@ import React from "react"
 
 const Login = () => {
     return (
-        <h2>Login</h2>
+        <div className="form-container">
+                <h2>Registro</h2>
+                <form>
+                  <label className="form-label">
+                    Correo electrónico:
+                    <input type="email" className="form-input" />
+                  </label>
+                  <br />
+                  <label className="form-label">
+                    Contraseña:
+                    <input type="password" className="form-input" />
+                  </label>
+                  <br />
+                  <button type="button" className="form-button"> Registrar </button>
+                </form>
+              </div>
     )
 }
 
