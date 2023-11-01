a8851f3a35a8e32f91e851906437c7d2f62d606a
EmiVivallo <emivivallozmm@gmail.com>
2023-10-31 21:40:56 -0300

    instalo routes y modifico app.js

--- los-perro-amigos/src/App.js
+++ los-perro-amigos/src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import './App.css';
 import AulasComponent from './componentes/AulasComponent'; 
 import CursosComponent from './componentes/CursosComponent';
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"
 
 function App() {
+  <BrowserRouter>
+    <NavLink to="/home"> CursosComponent </NavLink>
+  </BrowserRouter>
   const handleButtonClick = () => {
-    alert('¡Botón personalizado clickeado!');
   };
 
 
   return (
+
     <div className="App">
-      <button onClick={handleButtonClick}>Mi Botón</button> {}
+     <BrowserRouter>
+    <NavLink to="/teoria"> <button onClick={handleButtonClick}>Teoria</button> {}</NavLink>
+    <NavLink to="/taller"> <button onClick={handleButtonClick}>Taller</button> {}</NavLink>
+  </BrowserRouter>
       <CursosComponent className="CursosComponent"/> {}
       <AulasComponent /> {}
     </div>
