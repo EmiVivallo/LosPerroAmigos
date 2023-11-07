05becf45cb8b8bbacb6dc52781b510c2e5ace651
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-11-07 14:36:32 -0300

    Creando onclick boton cerrar sesion

--- los_perro_amigo/src/componentes/Home.js
+++ los_perro_amigo/src/componentes/Home.js
@@ -1,8 +1,12 @@
 import React from "react"
+import appFirebase from "../firebaseConfig";
+import { getAuth, signOut } from "firebase/auth";
+
+const auth = getAuth(appFirebase)
 
 const Home = ({correoUsuario}) => {
     return (
-        <h2 className="h22">Bienvenido usuario {correoUsuario} <button onClick={}> Cerrar Sesion</button> </h2>
+        <h2 className="h22">Bienvenido usuario {correoUsuario} <button onClick={()=>signOut(auth)}> Cerrar Sesion</button> </h2>
     )
 }
 
