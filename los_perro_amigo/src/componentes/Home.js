b67cab2628484830482d9b5ec5778850d1962317
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-11-07 14:34:32 -0300

    Creando boton cerrar sesion

--- los_perro_amigo/src/componentes/Home.js
+++ los_perro_amigo/src/componentes/Home.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Home = ({correoUsuario}) => {
     return (
-        <h2 className="h22">Bienvenido usuario {correoUsuario} </h2>
+        <h2 className="h22">Bienvenido usuario {correoUsuario} <button onClick={}> Cerrar Sesion</button> </h2>
     )
 }
 
