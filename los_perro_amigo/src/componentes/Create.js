9e46a8213e55348994de596bd03bf034734adf3b
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-11-14 12:13:40 -0300

    Constantes create

--- los_perro_amigo/src/componentes/Create.js
+++ los_perro_amigo/src/componentes/Create.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, addDoc } from 'firebase/firestore'
+
+import { db } from '../firebaseConfig'
 
 const Create = () => {
+  const [ aula, setAula ] = useState(0)
+  const [ materia, setMateria ] = useState('')
+  const navigate = useNavigate()
+
   return (
     <div>Create</div>
   )
