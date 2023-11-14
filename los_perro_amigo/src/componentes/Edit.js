c1930d7200b15480573605882352b01a2ebc493a
MatiasPizarro024 <103393286+MatiasPizarro024@users.noreply.github.com>
2023-11-14 13:05:50 -0300

    Agregando funciones edit

--- los_perro_amigo/src/componentes/Edit.js
+++ los_perro_amigo/src/componentes/Edit.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getDoc, updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebaseConfig'
+
 
 const Edit = () => {
+  const [ aula, setAula ] = useState(0)
+  const [ materia, setMateria ] = useState('')
+
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+  const update = async (e) => {
+
+  }
+
+  const getProductById = async (id) => {
+
+  }
+
+  useEffect( () => {
+    getProductById(id)
+  }, [ ])
+
   return (
     <div>Edit</div>
   )
