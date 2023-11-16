aeb4947433317075fdeeb74b0089e8215b6a3423
EmiVivallo <emivivallozmm@gmail.com>
2023-11-16 11:16:11 -0300

    Editando baja de informacion

--- los_perro_amigo/src/componentes/Show.js
+++ los_perro_amigo/src/componentes/Show.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { collection, getDocs, deleteDoc, doc, orderBy, query } from 'firebase/firestore';
+import { collection, getDocs, updateDoc, doc, orderBy, query } from 'firebase/firestore';
 import { db } from "../firebaseConfig";
 import Swal from 'sweetalert2';
 import withReactContent from "sweetalert2-react-content";
@@ -8,16 +8,12 @@ import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 
 export const Show = () => {
-  //hooks
   const [products, setProducts] = useState([]);
 
-  //database
   const productsCollection = collection(db, "products");
 
-  //mostrar
   const getProducts = async () => {
     const q = query(productsCollection, orderBy('aula'));
-
     const data = await getDocs(q);
 
     setProducts(
@@ -25,75 +21,99 @@ export const Show = () => {
     );
   };
 
-  //eliminar
-  const deleteProduct = async (id) => {
+  const updateProductStatus = async (id, isActive) => {
     const productDoc = doc(db, 'products', id);
-    await deleteDoc(productDoc);
+    await updateDoc(productDoc, { activo: isActive });
     getProducts();
   };
 
-  //confirmacion alert
-  const confirmDelete = (id) => {
+  const deactivateProduct = (id) => {
     MySwal.fire({
-      title: '¿Borrar la clase?',
-      text: 'No podrás revertir los cambios',
+      title: '¿Desactivar la clase?',
+      text: 'Puedes activarla nuevamente más tarde',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#d33',
       cancelButtonColor: '#3085d6',
-      confirmButtonText: "Sí, eliminar"
+      confirmButtonText: "Sí, desactivar"
     }).then((result) => {
       if (result.isConfirmed) {
-        deleteProduct(id);
+        updateProductStatus(id, false);
         Swal.fire(
-          'Eliminada',
-          'Clase eliminada',
+          'Desactivada',
+          'Clase desactivada',
           'success'
         );
       }
     });
   };
 
-  //useeffect
+  const activateProduct = (id) => {
+    MySwal.fire({
+      title: '¿Activar la clase?',
+      text: 'Puedes desactivarla nuevamente más tarde',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#28a745',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: "Sí, activar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        updateProductStatus(id, true);
+        Swal.fire(
+          'Activada',
+          'Clase activada',
+          'success'
+        );
+      }
+    });
+  };
+
   useEffect(() => {
     getProducts();
   }, []);
 
   return (
     <div className='main-container'>
-    <div className='container-show'>
-      <div className='row'>
-        <div className='col'>
-          <div className='d-grid gap-2'>
-            <Link to="/" className='btn btn-info'>Home</Link>
-            <Link to="/create" className='btn btn-secondary mb-1'>Create</Link>
-          </div>
+      <div className='container-show'>
+        <div className='row'>
+          <div className='col'>
+            <div className='d-grid gap-2'>
+              <Link to="/" className='btn btn-info'>Home</Link>
+              <Link to="/create" className='btn btn-secondary mb-1'>Create</Link>
+            </div>
 
-          <table className='table table-dark table-hover'>
-            <thead>
-              <tr>
-                <th>Aulas</th>
-                <th>Materias</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {products.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.aula}</td>
-                  <td>{product.materia}</td>
-                  <td>
-                    <Link to={`/edit/${product.id}`} className='btn btn-light'><i className='fa-solid fa-pencil'></i></Link>
-                    <button onClick={() => { confirmDelete(product.id) }} className='btn btn-danger'><i className='fa-solid fa-trash'></i></button>
-                  </td>
+            <table className='table table-dark table-hover'>
+              <thead>
+                <tr>
+                  <th>Aulas</th>
+                  <th>Materias</th>
+                  <th>Acciones</th>
                 </tr>
-              ))}
+              </thead>
+
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product.id} className={product.activo ? '' : 'table-danger bg-more-red'}>
+                    <td style={{ textDecoration: product.activo ? 'none' : 'line-through' }}>{product.aula}</td>
+                    <td style={{ textDecoration: product.activo ? 'none' : 'line-through' }}>{product.materia}</td>
+                    <td>
+                     {product.activo ? (
+                      <>
+                      <Link to={`/edit/${product.id}`} className='btn btn-light'><i className='fa-solid fa-pencil'></i></Link>
+                        <button onClick={() => { deactivateProduct(product.id) }} className='btn btn-warning'><i className='fa-solid fa-pause'></i></button>
+                      </>
+                      ) : (
+                        <button onClick={() => { activateProduct(product.id) }} className='btn btn-success'><i className='fa-solid fa-play'></i></button>
+                      )}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
-          </table>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
